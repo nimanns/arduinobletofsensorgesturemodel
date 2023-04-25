fd461dff8a21e9449745eff49d7c0a13aa6c8ccb
nimanns <qwerty.nma@gmail.com>
2023-04-25 17:09:07 -0400

    changed code formatting

--- lightsensorprediction.ino
+++ lightsensorprediction.ino
@@ -7,21 +7,19 @@
 #include "Adafruit_VL53L0X.h"
 
 namespace {
-tflite::ErrorReporter* error_reporter = nullptr;
-const tflite::Model* model = nullptr;
-tflite::MicroInterpreter* interpreter = nullptr;
-TfLiteTensor* model_input = nullptr;
-TfLiteTensor* model_output = nullptr;
-constexpr int kTensorArenaSize = 5 * 1024;
-uint8_t tensor_arena[kTensorArenaSize];
+  tflite::ErrorReporter* error_reporter = nullptr;
+  const tflite::Model* model = nullptr;
+  tflite::MicroInterpreter* interpreter = nullptr;
+  TfLiteTensor* model_input = nullptr;
+  TfLiteTensor* model_output = nullptr;
+  constexpr int kTensorArenaSize = 5 * 1024;
+  uint8_t tensor_arena[kTensorArenaSize];
 }
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
-
 void setup() {
-  while (!Serial)
-    ;
+  while (!Serial);
 
   static tflite::MicroErrorReporter micro_error_reporter;
   error_reporter = &micro_error_reporter;
@@ -40,22 +38,17 @@ void setup() {
   TfLiteStatus allocate_status = interpreter->AllocateTensors();
   if (allocate_status != kTfLiteOk) {
     error_reporter->Report("Allocate Tensor failed");
-    while (1)
-      ;
+    while (1);
   }
 
   model_input = interpreter->input(0);
   model_output = interpreter->output(0);
 
-  Serial.println(model_input->dims->size);
-  Serial.println(model_input->dims->data[0]);
-  Serial.println(model_input->dims->data[1]);
-  Serial.println(model_input->type);
   if (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
-    while (1)
-      ;
+    while (1);
   }
+  
   lox.startRangeContinuous();
 }
 
